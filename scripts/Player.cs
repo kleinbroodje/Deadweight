31fdf7029a81247fd92e903cff1eece185bf93e7
Daniel Tasada <daniteeem@gmail.com>
2025-05-27 22:41:01 +0200

    ship!!!!

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,57 +3,63 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
-	public const float Sens = 0.003f;
+    public const float Speed = 5.0f;
+    public const float JumpVelocity = 4.5f;
+    public const float Sens = 0.003f;
+    public const float floatForce = 1.0f;
+    public const float waterHeight = 1.0f;
+    public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
-	Camera3D camera;
+    Camera3D camera;
 
-	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+    public override void _Ready()
+    {
+        camera = GetNode<Camera3D>("Camera3D");
+    }
 
-	public override void _Ready()
-	{
-		camera = GetNode<Camera3D>("Camera3D");
-	}
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion motion)
+        {
+            RotateY(-motion.Relative.X * Sens);
+            camera.RotateX(-motion.Relative.Y * Sens);
+            camera.Rotation = new Vector3(Mathf.Clamp(camera.Rotation.X, Mathf.DegToRad(-40), Mathf.DegToRad(60)), camera.Rotation.Y, camera.Rotation.Z);
+        }
+    }
 
-	public override void _UnhandledInput(InputEvent @event)
-	{
-		if (@event is InputEventMouseMotion motion)
-		{
-			RotateY(-motion.Relative.X * Sens);
-			camera.RotateX(-motion.Relative.Y * Sens);
-			camera.Rotation = new Vector3(Mathf.Clamp(camera.Rotation.X, Mathf.DegToRad(-40), Mathf.DegToRad(60)), camera.Rotation.Y, camera.Rotation.Z);
-		}
-	}
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector3 velocity = Velocity;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector3 velocity = Velocity;
+        // Add the gravity.
+        // if (!IsOnFloor())
+        // 	velocity.Y -= gravity * (float)delta;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y -= gravity * (float)delta;
+        var depth = waterHeight - GlobalPosition.Y;
+        if (depth > 0)
+            Velocity += Vector3.Up * floatForce * gravity * depth;
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("jump") && IsOnFloor())
-			velocity.Y = JumpVelocity;
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
+        // Handle Jump.
+        if (Input.IsActionJustPressed("jump") && IsOnFloor())
+            velocity.Y = JumpVelocity;
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+        // Get the input direction and handle the movement/deceleration.
+        // As good practice, you should replace UI actions with custom gameplay actions.
+        Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+        if (direction != Vector3.Zero)
+        {
+            velocity.X = direction.X * Speed;
+            velocity.Z = direction.Z * Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+            velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+        }
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
 }
