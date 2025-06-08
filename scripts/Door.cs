384c96f69defa621cb34a2223763aaebeb49478a
Nathan Tse <nathantse2007@icloud.com>
2025-06-09 01:43:27 +0200

    added trapdoor

--- scripts/Door.cs
+++ scripts/Door.cs
@@ -9,6 +9,11 @@ public partial class Door : StaticBody3D, IDescription
     CharacterBody3D player;
     float maxAngle;
     float angleVel;
+    //what axis to rotate around, set to 1 if you want to rotate around that axis, else 0
+    float x;
+    float y;
+    float z;
+
 
     public override void _Ready()
     {
@@ -16,10 +21,13 @@ public partial class Door : StaticBody3D, IDescription
         hinge = GetNode<Node3D>("..");
         maxAngle = (float)GetMeta("maxAngle");
         angleVel = (float)GetMeta("angleVel");
+        x = (float)GetMeta("x");
+        y = (float)GetMeta("y");
+        z = (float)GetMeta("z");
     }
     public string Description()
     {
-        return "Press <E> to open or close the door";
+        return "Press <E> to open or close";
     }
     public override void _Process(double delta)
     {
@@ -31,16 +39,16 @@ public partial class Door : StaticBody3D, IDescription
         }
         if (toggle)
         {
-            if (hinge.Rotation.Y <= Mathf.DegToRad(maxAngle))
+            if (hinge.Rotation.Y <= Mathf.DegToRad(maxAngle) && hinge.Rotation.X <= Mathf.DegToRad(maxAngle) && hinge.Rotation.Z <= Mathf.DegToRad(maxAngle))
             {
-                hinge.Rotation += new Vector3(0, Mathf.DegToRad(angleVel), 0);
+                hinge.Rotation += new Vector3(Mathf.DegToRad(angleVel) * x, Mathf.DegToRad(angleVel) * y, Mathf.DegToRad(angleVel) * z);
             }
         }
         else
         {
-            if (hinge.Rotation.Y > 0)
+            if (hinge.Rotation.Y > 0 || hinge.Rotation.X > 0 || hinge.Rotation.Z > 0)
             {
-                hinge.Rotation -= new Vector3(0, Mathf.DegToRad(angleVel), 0);
+                hinge.Rotation -= new Vector3(Mathf.DegToRad(angleVel) * x, Mathf.DegToRad(angleVel) * y, Mathf.DegToRad(angleVel) * z);
             }
             else
             {
